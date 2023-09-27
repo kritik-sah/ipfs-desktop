f918e129acfe3c7377d63378828bd6ac79cfdde9
kritik sah <57076429+kritik-sah@users.noreply.github.com>
2023-09-27 13:38:59 +0530

    "View on IPFS gateway" opens link twice#2350
    fixed "View on IPFS gateway" opens link twice#2350

--- src/webui/open-external.js
+++ src/webui/open-external.js
@@ -2,18 +2,30 @@ const { app, shell } = require('electron')
 
 module.exports = function () {
   app.on('web-contents-created', (_, contents) => {
+    let linkOpened = false; // Initialize a flag to track whether the link has been opened
+
     contents.on('will-navigate', (event, url) => {
       const parsedUrl = new URL(url)
 
       if (parsedUrl.origin !== 'webui://-') {
-        event.preventDefault()
-        shell.openExternal(url)
+        event.preventDefault();
+
+        // Check if the link has already been opened
+        if (!linkOpened) {
+          shell.openExternal(url);
+          linkOpened = true; // Set the flag to true
+        }
       }
     })
 
     contents.on('new-window', (event, url) => {
-      event.preventDefault()
-      shell.openExternal(url)
+      event.preventDefault();
+
+      // Check if the link has already been opened
+      if (!linkOpened) {
+        shell.openExternal(url);
+        linkOpened = true; // Set the flag to true
+      }
     })
   })
 }
